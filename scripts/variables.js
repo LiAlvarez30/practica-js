4810b4e351947120bf0dd3aae80aad7290939913
Liliana Alvarez <lialvarez0630@gmail.com>
2025-04-23 14:00:33 -0300

    Se creo carpeta script e img para los archivos e imagenes, dentro de la carpeta script se creo el archivo variables.js para ver los diferentes comportamientos de cada variable

---
+++ scripts/variables.js
@@ -0,0 +1,43 @@
+// DECLARACION DE VARIABLES
+// VARIABLE VAR 
+
+var miVar = 'Texto de miVar';
+console.log(miVar);
+console.log(typeof miVar);
+
+var miVar = 'Mi nuevo miVar';
+console.log(miVar);
+
+miVar = 1991;
+console.log(miVar)
+console.log(typeof miVar);
+
+// VARIABLE LEF
+
+let milet;
+console.log(milet);
+
+milet = 'Texto de milet'
+console.log(milet);
+console.log(typeof milet);
+
+// let milet = 1991 *la variable LET no se puede redeclarar*
+
+
+
+// VARIABLE CONST
+
+//const miConst; *las variables const deben inicializarse*
+
+const miConst = 'Texto miConst';
+console.log(miConst);
+console.log(typeof miConst);
+
+//miConst = 1991;
+// console.log(miConst);      la variable const no permite reasignar
+// console.log(typeof miConst);
+
+
+// const miConst = 1991;
+// console.log(miConst);       la variable const no se puede redeclarar
+// console.log(typeof miConst);
